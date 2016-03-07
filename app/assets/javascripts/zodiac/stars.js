13c84ddf11ac3a48e8aacbbb2dedff54074bbbb5
irudym <irudym@gmail.com>
2016-03-07 21:28:28 +0300

    Fixed script
    Remove all functions from light.js and moved them to stars.js

--- app/assets/javascripts/zodiac/stars.js
+++ app/assets/javascripts/zodiac/stars.js
@@ -1,3 +1,55 @@
+function drawDivCircle(element, x, y, color) {
+    circle = $("<div class='circleBase type1'></div>");
+    circle.css({top: y, left: x, display: "none"});
+    element.append(circle);
+    return circle;
+}
+
+function getYsin(x) {
+    return height/2-Math.sin(x/(width/12))*(height/10)
+}
+
+
+
+function drawString() {
+    //check if canvas exists
+    if($("#canvas").length ) {
+        console.log("String of lights");
+        //draw string of lights
+
+        width = $("#canvas").width();
+        height = $("#canvas").height();
+
+        scale_f = width/630;
+        lights = 8;
+        ctx = $("#canvas")[0].getContext('2d');
+
+
+        function drawBulbs(element, x, y) {
+            if(x>630) return;
+            circle = drawDivCircle($("#canvas").parent(), x, getYsin(x)+10, "#AA3030");
+            circle.fadeIn(800, function() {
+                new_x = x+(630/lights) *scale_f;
+                drawBulbs(element,new_x, getYsin(new_x));
+            });
+        }
+
+        //draw sin line
+        ctx.moveTo(0,height/2);
+        for(i = 0;i < width;i++) {
+            ctx.lineTo(i,getYsin(i));
+        }
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#BABABA";
+        ctx.stroke();
+
+        //draw light bulbs
+        position = 20;
+        drawBulbs($("#canvas").parent(), position, getYsin(position));
+    }
+}
+
 
 
 function documentReady() {
@@ -9,6 +61,8 @@ function documentReady() {
             $(location).attr('href','/zodiac/light');
         });
     }
+
+    drawString();
 
     //if l == 0 them moveTo else lineTo
     pisces = [{x:68, y: 71, l:0}, {x:70, y: 49, l:1}, {x: 61, y: 58, l:1}, {x: 68,y: 71, l:1}, {x: 50, y: 108,l:1}, {x:33, y:135,l:1}, {x: 18, y: 161, l:1},
@@ -127,6 +181,8 @@ $.fn.preload = function() {
        $('<img/>')[0].src = this;
     });
 }
+
+
 
 $(document).ready(documentReady);
 $(document).on('page:load', documentReady);
